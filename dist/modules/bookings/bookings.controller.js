d7638f74fc550ff7ad183c91f029dcb1c1a4b287
kishore1999-commits <80811374+kishore1999-commits@users.noreply.github.com>
2026-06-10 14:19:19 -0700

    Added app.module.d.ts

---
+++ dist/modules/bookings/bookings.controller.js
@@ -0,0 +1,102 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BookingsController = void 0;
+const common_1 = require("@nestjs/common");
+const swagger_1 = require("@nestjs/swagger");
+const client_1 = require("@prisma/client");
+const bookings_service_1 = require("./bookings.service");
+const dto_1 = require("./dto");
+const guards_1 = require("../../common/guards");
+const decorators_1 = require("../../common/decorators");
+const dto_2 = require("../../common/dto");
+let BookingsController = class BookingsController {
+    constructor(bookingsService) {
+        this.bookingsService = bookingsService;
+    }
+    create(userId, dto) {
+        return this.bookingsService.create(userId, dto);
+    }
+    verifyPayment(dto) {
+        return this.bookingsService.verifyPayment(dto);
+    }
+    findMyBookings(userId, pagination) {
+        return this.bookingsService.findMyBookings(userId, pagination);
+    }
+    findAll(pagination) {
+        return this.bookingsService.findAll(pagination);
+    }
+    cancel(id, userId, role, dto) {
+        const isAdmin = role === client_1.Role.HOTEL_ADMIN || role === client_1.Role.SUPER_ADMIN;
+        return this.bookingsService.cancel(id, userId, dto, isAdmin);
+    }
+};
+exports.BookingsController = BookingsController;
+__decorate([
+    (0, common_1.Post)(),
+    (0, common_1.UseGuards)(guards_1.JwtAuthGuard),
+    (0, swagger_1.ApiOperation)({ summary: 'Create a booking with Razorpay order' }),
+    __param(0, (0, decorators_1.CurrentUser)('id')),
+    __param(1, (0, common_1.Body)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String, dto_1.CreateBookingDto]),
+    __metadata("design:returntype", void 0)
+], BookingsController.prototype, "create", null);
+__decorate([
+    (0, common_1.Post)('verify-payment'),
+    (0, common_1.UseGuards)(guards_1.JwtAuthGuard),
+    (0, swagger_1.ApiOperation)({ summary: 'Verify Razorpay payment and confirm booking' }),
+    __param(0, (0, common_1.Body)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [dto_1.VerifyPaymentDto]),
+    __metadata("design:returntype", void 0)
+], BookingsController.prototype, "verifyPayment", null);
+__decorate([
+    (0, common_1.Get)('my-bookings'),
+    (0, common_1.UseGuards)(guards_1.JwtAuthGuard),
+    (0, swagger_1.ApiOperation)({ summary: 'Get my bookings' }),
+    __param(0, (0, decorators_1.CurrentUser)('id')),
+    __param(1, (0, common_1.Query)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String, dto_2.PaginationDto]),
+    __metadata("design:returntype", void 0)
+], BookingsController.prototype, "findMyBookings", null);
+__decorate([
+    (0, common_1.Get)(),
+    (0, common_1.UseGuards)(guards_1.JwtAuthGuard, guards_1.RolesGuard),
+    (0, decorators_1.Roles)(client_1.Role.HOTEL_ADMIN, client_1.Role.SUPER_ADMIN),
+    (0, swagger_1.ApiOperation)({ summary: 'Get all bookings (Admin)' }),
+    __param(0, (0, common_1.Query)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [dto_2.PaginationDto]),
+    __metadata("design:returntype", void 0)
+], BookingsController.prototype, "findAll", null);
+__decorate([
+    (0, common_1.Delete)(':id'),
+    (0, common_1.UseGuards)(guards_1.JwtAuthGuard),
+    (0, swagger_1.ApiOperation)({ summary: 'Cancel a booking' }),
+    __param(0, (0, common_1.Param)('id')),
+    __param(1, (0, decorators_1.CurrentUser)('id')),
+    __param(2, (0, decorators_1.CurrentUser)('role')),
+    __param(3, (0, common_1.Body)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String, String, String, dto_1.CancelBookingDto]),
+    __metadata("design:returntype", void 0)
+], BookingsController.prototype, "cancel", null);
+exports.BookingsController = BookingsController = __decorate([
+    (0, swagger_1.ApiTags)('Bookings'),
+    (0, common_1.Controller)('bookings'),
+    __metadata("design:paramtypes", [bookings_service_1.BookingsService])
+], BookingsController);
+//# sourceMappingURL=bookings.controller.js.map
