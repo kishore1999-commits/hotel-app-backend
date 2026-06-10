2b2400e9bfb33c1ec9023f54e59f2af15b8cb0ef
kishore1999-commits <80811374+kishore1999-commits@users.noreply.github.com>
2026-06-10 14:35:28 -0700

    Deleted app.module.js

--- dist/modules/reviews/dto/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateReviewDto = void 0;
-var create_review_dto_1 = require("./create-review.dto");
-Object.defineProperty(exports, "CreateReviewDto", { enumerable: true, get: function () { return create_review_dto_1.CreateReviewDto; } });
-//# sourceMappingURL=index.js.map
