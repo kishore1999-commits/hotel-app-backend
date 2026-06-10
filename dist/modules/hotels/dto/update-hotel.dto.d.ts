19d7229c818f9c0b432e0b982893027a7a469f7b
kishore1999-commits <80811374+kishore1999-commits@users.noreply.github.com>
2026-06-10 14:36:52 -0700

    Deleted main.d.ts

--- dist/modules/hotels/dto/update-hotel.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateHotelDto } from './create-hotel.dto';
-declare const UpdateHotelDto_base: import("@nestjs/common").Type<Partial<CreateHotelDto>>;
-export declare class UpdateHotelDto extends UpdateHotelDto_base {
-}
-export {};
