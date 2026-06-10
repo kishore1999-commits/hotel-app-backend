d319611d506d14415d119f8b1d73070640da6479
kishore1999-commits <80811374+kishore1999-commits@users.noreply.github.com>
2026-06-10 14:38:25 -0700

    Deleted tsconfig.build.tsbuildinfo

--- dist/modules/rooms/dto/update-room.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateRoomDto } from './create-room.dto';
-declare const UpdateRoomDto_base: import("@nestjs/common").Type<Partial<Omit<CreateRoomDto, "hotelId">>>;
-export declare class UpdateRoomDto extends UpdateRoomDto_base {
-}
-export {};
--- dist/modules/transactions/transactions.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaService } from '../../prisma/prisma.service';
-export declare class TransactionsService {
-    private prisma;
-    constructor(prisma: PrismaService);
-    findByBookingId(bookingId: string): Promise<any>;
-}
