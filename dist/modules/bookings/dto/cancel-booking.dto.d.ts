e1f7f82b3c5dcd7721549ef89bdda59a80254055
kishore1999-commits <80811374+kishore1999-commits@users.noreply.github.com>
2026-06-10 14:19:15 -0700

    Deleted app.module.d.ts

--- dist/modules/bookings/dto/cancel-booking.dto.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class CancelBookingDto {
-    cancellationReason?: string;
-}
--- dist/modules/hotels/dto/query-hotel.dto.d.ts
+++
@@ -1,8 +0,0 @@
-import { PaginationDto } from '../../../common/dto';
-export declare class QueryHotelDto extends PaginationDto {
-    search?: string;
-    city?: string;
-    country?: string;
-    minRating?: number;
-    amenity?: string;
-}
